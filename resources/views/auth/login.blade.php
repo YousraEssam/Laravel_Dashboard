4f2f53a2711d3e80bc12129ce461609bdb7bb59a
YousraEssam <yousraa.essam.15@hotmail.com>
2019-09-09 18:20:44 +0200

    add login using email or phone number

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -33,7 +33,7 @@
             <form class="m-t" role="form" method="POST" action="{{ route('login') }}">
                 @csrf
                 <div class="form-group">
-                    <input type="email" class="form-control @error('email') is-invalid @enderror" 
+                    <input type="text" class="form-control @error('email') is-invalid @enderror" 
                             placeholder="Email or Phone Number" name="email" required="" autocomplete="email" autofocus>
                     @error('email')
                         <span class="invalid-feedback" role="alert">
