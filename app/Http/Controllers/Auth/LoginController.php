9c8f13a8d7a12387aafdf6ed215c9c1ee68468a5
YousraEssam <yousraa.essam.15@hotmail.com>
2019-09-25 19:23:21 +0200

    redirect all to landing page temporarily

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -29,7 +29,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/';
     protected $max_attempts = 2;
     /**
      * Create a new controller instance.
