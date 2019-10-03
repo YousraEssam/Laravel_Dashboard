ec2117799ed4842ba646ecb66ec146ab58f7c3e7
YousraEssam <yousraa.essam.15@hotmail.com>
2019-10-03 10:48:03 +0200

    edit in activity button

--- resources/views/visitors/activity.blade.php
+++ resources/views/visitors/activity.blade.php
@@ -1,6 +1,6 @@
 <td>
     <div class="toggle-btn @if($row->is_active) active @endif">
-        <input type="checkbox" @if($row->is_active) checked @endif 
+        <input id="{{$row->id}}" type="checkbox" @if($row->is_active) checked @endif 
             class="cb-value" onclick="return confirm('Are You Sure?')" />
         <span class="round-btn"></span>
     </div>
@@ -8,7 +8,7 @@
 
 <script>
 
-$('.cb-value').click(function() {
+$('#{{$row->id}}').click(function() {
     var mainParent = $(this).parent('.toggle-btn');
     if($(mainParent).find('input.cb-value').is(':checked')) {
         $(mainParent).addClass('active');
