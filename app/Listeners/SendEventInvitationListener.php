f9e7399acac19218e016999d4ebee1830cabd7a2
YousraEssam <yousraa.essam.15@hotmail.com>
2019-10-21 13:45:47 +0200

    implements shouldQueue

--- app/Listeners/SendEventInvitationListener.php
+++ app/Listeners/SendEventInvitationListener.php
@@ -9,7 +9,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Support\Facades\Mail;
 
-class SendEventInvitationListener
+class SendEventInvitationListener implements ShouldQueue
 {
     /**
      * Handle the event.
